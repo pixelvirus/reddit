f7dac0e573ccbe56f36e22ef419de7f517ea65ba
David Song <dsongx@gmail.com>
2014-01-26 18:21:01 -0800

    add keyword search in post title add comments

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,12 +1,15 @@
 import praw
-import time
 import re
+import time
 
 # access Reddit with user_agent
 reddit = praw.Reddit("Subreddit Monitor by github.com/pixelvirus/")
 
 # login
-reddit.login()
+#username = raw_input("Username? ")
+#password = raw_input("\nPassword? ")
+#print "\nLogging in...\n"
+reddit.login()#(username, password)
 
 # obtain subreddit
 sr = raw_input("\nWhich subreddit to monitor? ")
@@ -19,7 +22,7 @@ wordlist = re.sub("[^\w]", " ",  keywords).split()
 # obtain number of posts to monitor
 number = raw_input("\nHow many top posts to monitor? ")
 
-print "\nBot is now monitoring r/{name}\n".format(name=sr)
+print "\nThe bot is now monitoring r/{name}\n".format(name=sr)
 
 # stores submission IDs that has already been processed
 completed = []
@@ -27,10 +30,16 @@ completed = []
 while True:
     subreddit = reddit.get_subreddit(sr)
     for submission in subreddit.get_hot(limit=number):
+        # returns submission text
         text = submission.selftext.lower()
-        found = any(string in text for string in wordlist)
+        # returns submission title
+        title = submission.selftext.lower()
+        # look for keyword in text and title
+        found = any(string in text or title for string in wordlist)
+        # send a message if found and not already sent
         if submission.id not in completed and found:
-            msg = "Post found matching keyword(s): {link}".format(link=submission.short_link)
+            msg = "Post matching keyword(s) found: {link}".format(link=submission.short_link)
             reddit.user.send_message('pixelvirus', msg)
+            print "\nPost matching keyword(s) found. Message sent to {name}\n".format(name=username)
             completed.append(submission.id)
     time.sleep(1800)
